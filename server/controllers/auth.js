f60e6adbabe363b8a7875e94f8f50da6254e9505
DaryllCulas <culasdaryll99@gmail.com>
2025-03-02 05:45:03 +0800

    Register User

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,27 @@
+import { createError } from "../utils/error.js";
+import { connectToDB } from "../utils/connect.js";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+
 export async function register(req, res, next) {
+    const data = req.body;
+    console.log(data);
+
+    if (!data?.email || !data?.password) {
+        return next(createError(400, "Missing Fields"))
+    }
+
+    await connectToDB();
+    const alreadyRegistered = await User.exists({ email: data.email });
+
+    if(alreadyRegistered) return next(createError(400, "User already exists"))
+    // res.send("Register");
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+    await newUser.save();
+    res.status(201).json("User Created Successfully");
 }
 
 export async function login(req, res, next) {
--- server/utils/connect.js
+++ server/utils/connect.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose"
+import dotenv from "dotenv";
 
-const connection = { isConnected:null }
+dotenv.config();
+
+const connection = { isConnected: null };
 
 export const connectToDB = async() => {
     try {
         if (connection.isConnected) {
             return;
         }
+        // console.log("MONGO_URI:", process.env.MONGO_URI); // Log the MONGO_URI to verify it's loaded correctly
         const db = await mongoose.connect(process.env.MONGO_URI);
-        connection.isConnected = db.connection[0].readyState;
+        connection.isConnected = db.connections[0].readyState;
+      
     } catch (error) {
         console.log("Couldn't connect with the database: ", error);
     }
 }
+
