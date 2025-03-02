bb809f32d1118d3faaebc60b4f241a0bfc8e42f7
DaryllCulas <culasdaryll99@gmail.com>
2025-03-02 22:45:31 +0800

    Cookie Based login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import { createError } from "../utils/error.js";
 import { connectToDB } from "../utils/connect.js";
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export async function register(req, res, next) {
     const data = req.body;
@@ -14,7 +15,10 @@ export async function register(req, res, next) {
     await connectToDB();
     const alreadyRegistered = await User.exists({ email: data.email });
 
-    if(alreadyRegistered) return next(createError(400, "User already exists"))
+    if(alreadyRegistered) {
+        
+        return next(createError(400, "User already exists"))
+    } 
     // res.send("Register");
 
     const salt = bcrypt.genSaltSync(10);
@@ -25,7 +29,42 @@ export async function register(req, res, next) {
 }
 
 export async function login(req, res, next) {
+    
+    const data = req.body;
+    console.log(data);
+
+    if(!data?.email || !data?.password) {
+        return next(createError(400, "Missing Fields"));
+
+    }
+
+    await connectToDB();
+
+    const user = await User.findOne({ email: data.email });
+    if(!user) {
+        return next(createError(400, "Invalid Credentials"));   
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+    
+    if(!isPasswordCorrect) {
+        return next(createError(400, "Invalid Credentials"));
+    }
+
+    const token = jwt.sign({ id:user._id}, process.env.JWT);
+
+    console.log(token);
+    // res.send('login route');
+    res.cookie("access_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        }
+    ).status(200).json("User Logged in");
+
+
 }
 
 export async function logout(req, res, next) {
+
+
 }
