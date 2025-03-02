136f7febbb110b67c12b77fdb1c5d63e49a9af0c
DaryllCulas <culasdaryll99@gmail.com>
2025-03-03 05:53:09 +0800

    Add Icons

---
+++ client/src/components/icons/DeleteIcons.jsx
@@ -0,0 +1,14 @@
+const DeleteIcons = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path d="M3 6h18v2H3V6zm2 3h14v12H5V9zm3 2v8h2v-8H8zm4 0v8h2v-8h-2zm4 0v8h2v-8h-2z"/>
+    </svg>
+);
+
+export default DeleteIcons;
---
+++ client/src/components/icons/TickIcons.jsx
@@ -0,0 +1,14 @@
+const TickIcons = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path fill="currentColor" d="M9 16.2l-4.2-4.2-1.4 1.4L9 19 21 7l-1.4-1.4z"/>
+    </svg>
+);
+
+export default TickIcons;
