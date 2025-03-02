ef661fa4f93f640d8b469f7744aae7fe42527390
DaryllCulas <culasdaryll99@gmail.com>
2025-03-03 03:02:53 +0800

    Register and Login UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Todos from './components/Todo.jsx';
+import Register from './components/Register.jsx';
+import Login from './components/Login.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Todos />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
+  </BrowserRouter>
   </StrictMode>,
 )
